d146e0c28292655b65b23c401e37218995748205
Faith Waigi <waigifaith@gmail.com>
2024-03-05 03:32:40 +0300

    init

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Taking input from the user
+name = input("What is your name?:")
+# Taking age input from the user
+age = int(input("How old are you?:"))
+# Taking location input from user
+location = tuple(input("You live in?:"))
+
+
+print("Hello" +name)
+print("I am" +str(age) +"years old")
+print("I live in" +str(location))
+
